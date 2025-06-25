679f915c64a27e62261497bbfcfa7b1852a997ef
Karthikgit7569 <ameraboinakarthik@gmail.com>
2025-06-25 11:21:05 +0530

    java script

---
+++ trivial.js
@@ -0,0 +1,28 @@
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+
+var botName = "Codey";
+var botLocation = "Internet";
+var favoriteLanguage = "JavaScript";
+
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+
+let codingFact = "Did you know that " + favoriteLanguage + " was created in just 10 days?";
+console.log(codingFact);
+
+
+codingFact = favoriteLanguage + " can run both in the browser and on the server!";
+console.log(codingFact);
+
+
+codingFact = favoriteLanguage + " is used by over 95% of websites!";
+console.log(codingFact);
+
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
