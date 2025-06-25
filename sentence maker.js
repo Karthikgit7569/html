c64182a6673c522c43a0af765e728cd4a50c7bdd
Karthikgit7569 <ameraboinakarthik@gmail.com>
2025-06-25 11:34:58 +0530

    java script

---
+++ sentence maker.js
@@ -0,0 +1,28 @@
+
+
+let adjective = "fierce";
+let noun = "dragon";
+let noun2 = "marshmallows";
+let place = "volcano";
+let adjective2 = "fiery";
+let verb = "angry";
+
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+
+console.log("First story: " + firstStory);
+
+
+adjective = "shy";
+noun = "unicorn";
+noun2 = "cupcakes";
+place = "meadow";
+adjective2 = "sparkly";
+verb = "embarrassed";
+
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+
+console.log("Second story: " + secondStory);
