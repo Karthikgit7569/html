e81b28e94bc105301bd1197e92c0092ffb9fec75
Karthikgit7569 <ameraboinakarthik@gmail.com>
2025-07-10 10:55:35 +0530

    k

--- index.js
+++ index.js
@@ -68,29 +68,90 @@
 
 
 
-let promise= new Promise((resolve, reject) => {
-    console.log("iam, a promise");
-    if(1<0){
-        resolve(123);
-    }
-    reject("something went wrong")
-}
-);
-function getData(dataid){
+// let promise= new Promise((resolve, reject) => {
+//     console.log("iam, a promise");
+//     if(1<0){
+//         resolve(123);
+//     }
+//     reject("something went wrong")
+// }
+// );
+// function getData(dataid){
+//     return new Promise((resolve, reject) => {
+//         setTimeout(()=>{
+//             console.log("Fetching data for id:", dataid);
+//             resolve("success");
+//         },2000);
+//     });
+// }
+// let result =getData(123);
+// result;
+// getData(101).then(result=>{
+//     console.log("First fetch", result);
+//     return getData(102);
+// })
+// .then(result=>{
+//     console.log("second fetch:", result);
+//     return getData(103);
+// })
+// .then(result =>{
+//     console.log("third fetch", result);
+
+// })
+// .catch(err=>{
+//     console.error("error occured", err);
+// });
+// const getPromise = () => {
+//     return new Promise((resolve, reject) => {
+//         console.log("I am a promise");
+//         reject("Something went wrong");
+
+//     });
+// };
+
+// let result = getPromise();
+
+// result.then(data => {
+//     console.log("Data:", data);
+// })
+// .catch(error => {
+//     console.error("Error occurred:", error);
+// });
+
+// function asyncFunc1(){
+//     return new Promise(function(resolve, reject)=>{
+//         setTimeout(()=>{}
+//     )
+//         })
+// }
+function getData(id) {
     return new Promise((resolve, reject) => {
-        setTimeout(()=>{
-            console.log("Fetching data for id:", dataid);
-            resolve("success");
-        },2000);
+        setTimeout(() => {
+            console.log("Fetching data for id:",id);
+            resolve(id);
+        },1000);
     });
 }
-let result =getData(123);
-result;
-getData(101).then(result=>{
-    console.log("First fetch", result);
-    return getData(102);
-})
-.then(result=>{
-    console.log("second fetch:", result);
-    return getData(103);
-})
+async function fetchData(){
+    try{
+        const data1 = await getData(101);
+        console.log("first fetch:",data1);
+        const data2 = await getData(102);
+        console.log("second fetch:", data2);
+        const data3 = await getData(103);
+        console.log("third fetch:",data3);
+    } catch(error){
+        console.error("Error occurred",error);
+    }
+}
+
+fetchData();
+async function getnextData(){
+    console.log("getting data 1...");
+    await getData(1);
+    console.log("getting data2...");
+    await getData(2);
+    console.log("getting data 3....");
+    await getData(3);
+}
+    getnextData();
